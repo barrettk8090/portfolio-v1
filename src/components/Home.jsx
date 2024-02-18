85fd944188a979c040329aa49adaa5fe8bee321b
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-18 10:20:20 -0700

    init

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Projects from './Projects';
+
+function Home(){
+    return (
+        <div>
+            <h1>Barrett Kowalsky</h1>
+            <h2>Full Stack Engineer</h2>
+            <Projects />
+        </div>
+    )
+
+}
+
+export default Home;
