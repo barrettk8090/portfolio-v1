7a56ffad04a250426f45873267ac70b0ebec217f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-19 10:01:30 -0700

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import Links from './Links';
 function Home(){
     return (
         <div>
-            <h1>Barrett Kowalsky</h1>
+            <h1 className="text-3xl font-bold underline">Barrett Kowalsky</h1>
             <h2>Full Stack Dev</h2>
             <Projects />
             <Links />
