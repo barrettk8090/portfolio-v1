6ea6edd9ef1709ca82dba206ce82e355c2d417d7
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-20 09:36:44 -0700

    copy updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ function Home(){
     return (
         <div className="mx-8">
             <h1 className="mt-8 text-4xl font-bold">Barrett Kowalsky</h1>
-            <h4 className="my-4 text-xl"> Full Stack Dev</h4>
-            <p className="my-4 font-normal">Creating unique, meaningful, and user-friendly experiences. Inspired by outer space. Constantly learning.</p>
+            <h4 className="my-4 text-xl"> Full Stack Developer</h4>
+            <p className="my-4 font-normal">Interested in creating unique, meaningful, and user-friendly experiences.<br/> Inspired by outer space. </p>
             <Projects />
             <Blog/>
             <Links />
